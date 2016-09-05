8bc604394f9723b56b5323567e4ca58194f1225a
Tim D. Smith <git@tim-smith.us>
2016-09-05 16:43:33 -0700

    Support split plates

--- R/reporting.R
+++ R/reporting.R
@@ -6,8 +6,22 @@ load_cqs = function(gene) {
 }
 
 load_all_cqs = function() {
-  genes = c("arg1", "il10", "nos2", "tnfa")
-  genes %>%
+  genes = c("arg1", "il10", "nos2", "tnfa", "mrc1-tgfb1", "chi3l3-retnla")
+  df = genes %>%
     lapply(load_cqs) %>%
     bind_rows
+
+  split_plate = function(df, target) {
+    targets = stringr::str_split_fixed(target, "-", 2) %>%
+      sapply(stringr::str_to_title)
+    within(df, {
+      Target[Target == target & Timepoint == "4h"] = targets[1]
+      Target[Target == target & Timepoint == "24h"] = targets[2]
+      Timepoint[Target == targets[2]] = "4h"
+    })
+  }
+
+  df %>%
+    split_plate("Mrc1-Tgfb1") %>%
+    split_plate("Chi3l3-Retnla")
 }
